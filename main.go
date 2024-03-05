dcd8730d3085d43893bf9cdfde88c2a4478541c7
abljiu <1160433376@qq.com>
2024-03-05 22:23:22 +0800

    gin study

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	// "fmt"
+	"database/sql"
+	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
+	"net/http"
+	// "golang.org/x/tools/go/analysis/passes/nilfunc"
+)
+
+func main() {
+	//创建server
+	r := gin.Default()
+	db, err := sql.Open("mysql", "username:password@tcp(127.0.0.1:3306)/dbname")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db.Close()
+
+	r.LoadHTMLGlob("templates/*")
+
+	r.GET("/main.go", func(ctx *gin.Context) {
+		ctx.HTML(http.StatusOK, "start.html", nil)
+	})
+	
+	// 定义一个路由，当请求 POST /login 时，接收 username 和 password，并返回接收到的数据
+	r.POST("/login", func(c *gin.Context) {
+		// 使用 c.PostForm() 方法获取表单提交的数据
+		username := c.PostForm("username")
+		password := c.PostForm("password")
+
+		
+		// 返回接收到的数据
+		c.HTML(http.StatusOK,"log_res.html", gin.H{
+			"username": username,
+			"password": password,
+		})
+	})
+
+	r.Run(":8000")
+}
