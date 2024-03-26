a213ce295494d578bcbd3b267ff9a7dbcae3d797
abljiu <1160433376@qq.com>
2024-03-26 22:53:17 +0800

    studentOS

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ import (
 func main() {
 	//创建server
 	r := gin.Default()
-	db, err := sql.Open("mysql", "username:password@tcp(127.0.0.1:3306)/dbname")
+	db, err := sql.Open("mysql", "username:password@tcp(10.30.0.208/:3306)/dbname")
 	if err != nil {
 		panic(err.Error())
 	}
@@ -21,7 +21,7 @@ func main() {
 	r.LoadHTMLGlob("templates/*")
 
 	r.GET("/main.go", func(ctx *gin.Context) {
-		ctx.HTML(http.StatusOK, "start.html", nil)
+		ctx.HTML(http.StatusOK, "login.html", nil)
 	})
 	
 	// 定义一个路由，当请求 POST /login 时，接收 username 和 password，并返回接收到的数据
@@ -38,5 +38,5 @@ func main() {
 		})
 	})
 
-	r.Run(":8000")
+	r.Run("10.30.0.208:8000")
 }
