70c4296007547118cae4e56dcd147ea5315a6992
abljiu <1160433376@qq.com>
2024-03-28 23:57:07 +0800

    Gin

---
+++ Data_base/Data_base.go
@@ -0,0 +1,17 @@
+package database
+
+import "fmt"
+
+// 在这里编写与数据库相关的代码
+
+func Connect() {
+	// 连接数据库的逻辑
+	fmt.Println("Connecting to the database...")
+}
+
+func Query() {
+	// 数据库查询的逻辑
+	fmt.Println("Executing a database query...")
+}
+
+//验证用户的密码是否正确
